8ddd47753c5d19672fec46fc5ff46a5da8f7156d
Daniil Basmanov <basmanovdaniil@gmail.com>
2018-09-18 11:48:48 +0300

    Updated Builder

--- Unity_ProceduralToolkit/Assets/ProceduralToolkit.Dev/Editor/Builder.cs
+++ Unity_ProceduralToolkit/Assets/ProceduralToolkit.Dev/Editor/Builder.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Reflection;
 using UnityEditor;
@@ -6,7 +7,49 @@ public static class Builder
 {
     private const string sceneNameTag = "SCENENAME";
     private const string versionTag = "VERSION";
-    private const string buildFolderKey = "PT_WebGL_BuildFolder";
+    private const string webglBuildFolderKey = "PT_WebGL_BuildFolder";
+    private const string androidBuildFolderKey = "PT_Android_BuildFolder";
+    private const string iosBuildFolderKey = "PT_iOS_BuildFolder";
+
+    [MenuItem("Procedural Toolkit/Build Android")]
+    public static void BuildAndroid()
+    {
+        string buildPath = GetBuildFile(androidBuildFolderKey, "ProceduralToolkit.apk", "apk");
+        if (string.IsNullOrEmpty(buildPath))
+        {
+            return;
+        }
+        EditorUtility.DisplayProgressBar("Building...", null, 0);
+
+        BuildPipeline.BuildPlayer(new BuildPlayerOptions
+        {
+            target = BuildTarget.Android,
+            locationPathName = buildPath,
+            scenes = Array.ConvertAll(EditorBuildSettings.scenes, s => s.path),
+        });
+
+        EditorUtility.ClearProgressBar();
+    }
+
+    [MenuItem("Procedural Toolkit/Build iOS")]
+    public static void BuildIOS()
+    {
+        string buildPath = GetBuildFolder(iosBuildFolderKey, "iOS");
+        if (string.IsNullOrEmpty(buildPath))
+        {
+            return;
+        }
+        EditorUtility.DisplayProgressBar("Building...", null, 0);
+
+        BuildPipeline.BuildPlayer(new BuildPlayerOptions
+        {
+            target = BuildTarget.iOS,
+            locationPathName = buildPath,
+            scenes = Array.ConvertAll(EditorBuildSettings.scenes, s => s.path),
+        });
+
+        EditorUtility.ClearProgressBar();
+    }
 
     [MenuItem("Assets/Build scene(s)", true)]
     public static bool BuildSceneTest()
@@ -25,16 +68,10 @@ public static class Builder
     [MenuItem("Assets/Build scene(s)", priority = 0)]
     public static void BuildScene()
     {
-        string buildFolder = EditorPrefs.GetString(buildFolderKey);
+        string buildFolder = GetBuildFolder(webglBuildFolderKey, "PTGitHubPages");
         if (string.IsNullOrEmpty(buildFolder))
         {
-            buildFolder = EditorUtility.OpenFolderPanel("Select the build folder", null, "PTGitHubPages");
-            if (string.IsNullOrEmpty(buildFolder))
-            {
-                return;
-            }
-
-            EditorPrefs.SetString(buildFolderKey, buildFolder);
+            return;
         }
 
         EditorUtility.DisplayProgressBar("Building...", null, 0);
@@ -62,13 +99,48 @@ public static class Builder
                 info.Delete(true);
             }
 
-            BuildPipeline.BuildPlayer(new[] {scenePath}, buildPath, BuildTarget.WebGL, BuildOptions.None);
+            BuildPipeline.BuildPlayer(new BuildPlayerOptions
+            {
+                target = BuildTarget.WebGL,
+                locationPathName = buildPath,
+                scenes = new[] {scenePath},
+            });
         }
 
         SetTemplateTag(sceneNameTag, "");
         SetTemplateTag(versionTag, "");
 
         EditorUtility.ClearProgressBar();
+    }
+
+    private static string GetBuildFolder(string key, string defaultName)
+    {
+        string buildPath = EditorPrefs.GetString(key);
+        if (string.IsNullOrEmpty(buildPath))
+        {
+            buildPath = EditorUtility.OpenFolderPanel("Select the build folder", null, defaultName);
+            if (string.IsNullOrEmpty(buildPath))
+            {
+                return null;
+            }
+            EditorPrefs.SetString(key, buildPath);
+        }
+        return buildPath;
+    }
+
+    private static string GetBuildFile(string key, string defaultName, string extension)
+    {
+        string buildPath = EditorPrefs.GetString(key);
+        //if (string.IsNullOrEmpty(buildPath))
+        {
+            buildPath = EditorUtility.SaveFilePanel("Select the build file", null, defaultName, extension);
+            if (string.IsNullOrEmpty(buildPath))
+            {
+                return null;
+            }
+            EditorPrefs.SetString(key, buildPath);
+        }
+        return buildPath;
     }
 
     private static void SetTemplateTag(string key, string value)
